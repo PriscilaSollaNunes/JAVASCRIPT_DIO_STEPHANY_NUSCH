29e879b48de70c76347f7c41bb19513cc7679c07
Seu nome <priscilasolla.ps@gmail.com>
2022-08-08 16:37:02 -0300

    add all files

---
+++ Modulo/main.mjs
@@ -0,0 +1,5 @@
+import  { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Priscila', '40'));
+console.log(mostraCidade('Priscila', 'São Paulo'));
+console.log(mostraHobby('Priscila', 'fazer exercício'));
